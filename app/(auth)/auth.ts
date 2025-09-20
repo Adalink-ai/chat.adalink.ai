d38c574833c5f949b4b3a24164c0b4aa6101767e
Wesley Santos <wesleysantos.0095@gmail.com>
2025-09-19 21:03:20 -0300

    fix: credentials

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -39,8 +39,13 @@ export const {
   ...authConfig,
   providers: [
     Credentials({
-      credentials: {},
-      async authorize({ email, password }: any) {
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const email = credentials.email as string;
+        const password = credentials.password as string;
         const users = await getUser(email);
 
         if (users.length === 0) {
