cfd9a5cc1f23ff51d6bc24cb89c27c9298dd9e5b
Fernando Rodrigues <fernando.hardd@gmail.com>
2025-10-08 14:44:50 -0300

    fix: add text-black class to email input for better contrast

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -27,7 +27,7 @@ export function AuthForm({
         <Input
           id="email"
           name="email"
-          className="bg-muted text-md md:text-sm"
+          className="bg-muted text-md md:text-sm text-black"
           type="email"
           placeholder="user@acme.com"
           autoComplete="email"
